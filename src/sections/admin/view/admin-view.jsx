2cd553188cfcc7a5696b54ecfa9c2ef990d5af3f
Bulat Nigmatullin <bulatnig@protonmail.com>
2024-01-27 10:19:58 +0000

    initial pagination implementation

--- src/sections/admin/view/admin-view.jsx
+++ src/sections/admin/view/admin-view.jsx
@@ -1,52 +1,48 @@
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
+import { useEffect, useState } from 'react';
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
+  { field: 'name', headerName: 'Name', width: 130 },
+  { field: 'country', headerName: 'Country', width: 130 },
+  { field: 'city', headerName: 'City', width: 130 },
+  { field: 'createdAt', headerName: 'Created At', type: 'datetime', width: 200 },
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 1, name: 'Snow', country: 'Jon' },
+  { id: 2, name: 'Lannister', country: 'Cersei' },
+  { id: 3, name: 'Lannister', country: 'Jaime' },
+  { id: 4, name: 'Stark', country: 'Arya' },
+  { id: 5, name: 'Targaryen', country: 'Daenerys' },
+  { id: 6, name: 'Melisandre', country: null },
+  { id: 7, name: 'Clifford', country: 'Ferrara' },
+  { id: 8, name: 'Frances', country: 'Rossini' },
+  { id: 9, name: 'Roxie', country: 'Harvey' },
 ];
 
 export default function AdminPage() {
+  const [tableData, setTableData] = useState([])
+  const [paginationModel, setPaginationModel] = React.useState({
+    pageSize: 10,
+    page: 0,
+  });
+  useEffect(() => {
+    fetch(`https://65b4c5bf41db5efd2866e486.mockapi.io/api/v1/users?page=${paginationModel.page + 1}&limit=${paginationModel.pageSize}`)
+      .then((data) => data.json())
+      .then((data) => setTableData(data))
+  }, [paginationModel])
+  console.log(tableData)
+
   return (
-    <div style={{ height: 400, width: '100%' }}>
+    <div style={{ height: '100%', width: '100%' }}>
       <DataGrid
-        rows={rows}
+        rows={tableData}
         columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
-          },
-        }}
-        pageSizeOptions={[5, 10]}
-        checkboxSelection
+        paginationMode='server'
+        paginationModel={paginationModel}
+        onPaginationModelChange={setPaginationModel}
       />
     </div>
   );
