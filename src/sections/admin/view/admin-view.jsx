f16d35698f116201fafc2593b299dd2795306d9f
Bulat Nigmatullin <bulatnig@protonmail.com>
2024-01-27 10:44:57 +0000

    add action column

--- src/sections/admin/view/admin-view.jsx
+++ src/sections/admin/view/admin-view.jsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
+
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 import { DataGrid } from '@mui/x-data-grid';
-import { useEffect, useState } from 'react';
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 70 },
@@ -8,6 +11,31 @@ const columns = [
   { field: 'country', headerName: 'Country', width: 130 },
   { field: 'city', headerName: 'City', width: 130 },
   { field: 'createdAt', headerName: 'Created At', type: 'datetime', width: 200 },
+  {
+    field: 'action',
+    headerName: 'Action',
+    width: 180,
+    sortable: false,
+    disableClickEventBubbling: true,
+
+    renderCell: (params) => {
+      const onEdit = (e) => {
+        const currentRow = params.row;
+        return alert(`Edit ${  JSON.stringify(currentRow, null, 4)}`);
+      };
+      const onDelete = (e) => {
+        const currentRow = params.row;
+        return alert(`Delete ${  JSON.stringify(currentRow, null, 4)}`);
+      };
+
+      return (
+        <Stack direction="row" spacing={2}>
+          <Button variant="outlined" color="warning" size="small" onClick={onEdit}>Edit</Button>
+          <Button variant="outlined" color="error" size="small" onClick={onDelete}>Delete</Button>
+        </Stack>
+      );
+    },
+  }
 ];
 
 const rows = [
